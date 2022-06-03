3529cff85020701679132b3f4f04fc44f8e392af
alexandre.girold <alexandre.girold@epita.fr>
2022-06-03 14:21:07 +0200

    movement

---
+++ Menu/main.js
@@ -0,0 +1,18 @@
+var elements = document.getElementsByClassName("topbar");
+
+function Menu1()
+{
+    window.location.href = "index.html";    
+}
+function Menu2()
+{
+    window.location.href = "about.html";    
+}
+function Menu3()
+{
+    window.location.href = "gallery.html";    
+}
+function Contact()
+{
+    window.location.href = "contact.html";    
+}
